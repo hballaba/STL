7a6ea13481903fc7b5c3da1175d05182771a0177
Howard Ballabar <hballaba@si-k6.kzn.21-school.ru>
2021-03-14 20:58:37 +0300

    first_commit

--- vector/vectorTest.cpp
+++ vector/vectorTest.cpp
@@ -1,108 +1,108 @@
-#include "ft_vector.hpp"
+#include "vector.hpp"
 #include <vector>
 
 int     main() {
     try {
 
 
-//   ft_vector<int> v1;
-//   ft_vector<int>::iterator it;
+	vector<int> myVec1;
+	vector<int>::iterator myIt;
 
-   std::vector<int> v1;
-   std::vector<int>::iterator it;
+	std::vector<int> originalVec1;
+	std::vector<int>::iterator originalIt;
 
 
 
-//    ft_vector<std::string> v2(5);
+	//    vector<std::string> v2(5);
 
- //   std::vector<int> v1;
-//    std::vector<std::string> v2(5);
+	//   std::vector<int> originalVec1;
+	//    std::vector<std::string> v2(5);
 
-    std::cout << v1.capacity() << std::endl;
-    v1.push_back(7);
-	v1.push_back(10);
-	v1.push_back(20);
-    v1.push_back(30);
-    // std::cout << "test size = " << v1.size() << std::endl;
-    // //v1.clear();
-    // std::cout << "test resize = " << v1.size() << std::endl;
-    
-    // v1.resize(5, 5);
-    // std::cout << "test resize. size = " << v1.size() << std::endl;
-    // for (int i = 0; i < v1.size(); i++)
-    //     std::cout << i << "= " << v1[i] << std::endl;
-    // v1.reserve(20);
-    // std::cout << "test resize. capasity = " << v1.capacity() << std::endl;
-    
-    // std::cout << "test resize. size = " << v1.size() << std::endl;
-    
-    // //std::cout << "at index = " << v1.at(5) << std::endl; // проверить на исключение
-    // std::cout << "test resize. size = " << v1.size() << std::endl;
-    // std::cout << "front index = " << v1.front() << std::endl;
-    // std::cout << "back index = " << v1.back() << std::endl;
-    // std::cout << "begin index = " << *v1.begin() << std::endl;
-    // std::cout << "end index = " << *v1.end() << std::endl;
-    // std::cout << "rbegin index = " << *v1.rbegin() << std::endl;
-    // std::cout << "rend index = " << *v1.rend() << std::endl;
+		std::cout << myVec1.capacity() << std::endl;
+		myVec1.push_back(7);
+		myVec1.push_back(10);
+		myVec1.push_back(20);
+		myVec1.push_back(30);
+		// std::cout << "test size = " << myVec1.size() << std::endl;
+		// //myVec1.clear();
+		// std::cout << "test resize = " << myVec1.size() << std::endl;
+		
+		// myVec1.resize(5, 5);
+		// std::cout << "test resize. size = " << myVec1.size() << std::endl;
+		// for (int i = 0; i < myVec1.size(); i++)
+		//     std::cout << i << "= " << myVec1[i] << std::endl;
+		// myVec1.reserve(20);
+		// std::cout << "test resize. capasity = " << myVec1.capacity() << std::endl;
+		
+		// std::cout << "test resize. size = " << myVec1.size() << std::endl;
+		
+		// //std::cout << "at index = " << myVec1.at(5) << std::endl; // проверить на исключение
+		// std::cout << "test resize. size = " << myVec1.size() << std::endl;
+		// std::cout << "front index = " << myVec1.front() << std::endl;
+		// std::cout << "back index = " << myVec1.back() << std::endl;
+		// std::cout << "begin index = " << *myVec1.begin() << std::endl;
+		// std::cout << "end index = " << *myVec1.end() << std::endl;
+		// std::cout << "rbegin index = " << *myVec1.rbegin() << std::endl;
+		// std::cout << "rend index = " << *myVec1.rend() << std::endl;
 
-    
-    // v1.assign(4, 5);
-    std::cout << "test asign. size = " << v1.capacity() << std::endl;
-    for (int i = 0; i < v1.size(); i++)
-        std::cout << i << "= " << v1[i] << std::endl;
+		
+		// myVec1.assign(4, 5);
+		std::cout << "test asign. size = " << myVec1.capacity() << std::endl;
+		for (int i = 0; i < myVec1.size(); i++)
+			std::cout << i << "= " << myVec1[i] << std::endl;
 
-    
-    
+		
+		
 
-    it = v1.begin();
-    it++;
-    // it = v1.insert(it , 200);
-    //v1.insert(it, 2, 300);
+		it = myVec1.begin();
+		it++;
+		// it = myVec1.insert(it , 200);
+		//myVec1.insert(it, 2, 300);
 
-    // it++;
-    // it++;
-    // it = v1.insert(it, v1.begin(), v1.end());
+		// it++;
+		// it++;
+		// it = myVec1.insert(it, myVec1.begin(), myVec1.end());
 
-    // std::cout << "test insert. size = " << v1.size() << std::endl;
-    // std::cout << "test insert it = " << *it << std::endl;
-    // for (int i = 0; i < v1.size(); i++)
-    //     std::cout << i << " = " << v1[i] << std::endl;
-    
+		// std::cout << "test insert. size = " << myVec1.size() << std::endl;
+		// std::cout << "test insert it = " << *it << std::endl;
+		// for (int i = 0; i < myVec1.size(); i++)
+		//     std::cout << i << " = " << myVec1[i] << std::endl;
+		
 
-    // it = v1.erase(it);
-    // //it = v1.erase(it, v1.end());
-    // std::cout << "test erase size = " << v1.size() << std::endl;
-    // std::cout << "test erase it = " << *it << std::endl;
-    // for (int i = 0; i < v1.size(); i++)
-    //     std::cout << i << " = " << v1[i] << std::endl;
-	std::cout << std::endl;
-    
-	ft_vector<int> v2, v3;
-	//std::vector<int> v2, v3;
+		// it = myVec1.erase(it);
+		// //it = myVec1.erase(it, myVec1.end());
+		// std::cout << "test erase size = " << myVec1.size() << std::endl;
+		// std::cout << "test erase it = " << *it << std::endl;
+		// for (int i = 0; i < myVec1.size(); i++)
+		//     std::cout << i << " = " << myVec1[i] << std::endl;
+		std::cout << std::endl;
+		
+		vector<int> v2, v3;
+		//std::vector<int> v2, v3;
 
-    // v2.resize(3, 1);
-    // v3.resize(4, 9);
-    // for (int i = 0; i < v2.size(); i++)
-    //     std::cout << "v1 " << i << " = " << v2[i] << std::endl;
-    // for (int i = 0; i < v3.size(); i++)
-    //     std::cout << "v2 " << i << " = " << v3[i] << std::endl;
+		// v2.resize(3, 1);
+		// v3.resize(4, 9);
+		// for (int i = 0; i < v2.size(); i++)
+		//     std::cout << "myVec1 " << i << " = " << v2[i] << std::endl;
+		// for (int i = 0; i < v3.size(); i++)
+		//     std::cout << "v2 " << i << " = " << v3[i] << std::endl;
 
-	// v2.swap(v3);
-	// std::cout << std::endl;
-	// for (int i = 0; i < v2.size(); i++)
-    //     std::cout << "v1 " << i << " = " << v2[i] << std::endl;
-    // for (int i = 0; i < v3.size(); i++)
-    //     std::cout << "v2 " << i << " = " << v3[i] << std::endl;
+		// v2.swap(v3);
+		// std::cout << std::endl;
+		// for (int i = 0; i < v2.size(); i++)
+		//     std::cout << "myVec1 " << i << " = " << v2[i] << std::endl;
+		// for (int i = 0; i < v3.size(); i++)
+		//     std::cout << "v2 " << i << " = " << v3[i] << std::endl;
 
-    std::cout << "v2 " <<  " = " << v2.max_size()  << std::endl;
+		std::cout << "v2 " <<  " = " << v2.max_size()  << std::endl;
 
-   	// v2.push_back("sdff");
-    // std::cout << v2.size() << std::endl;
-	// v2.push_back("sdff");
-    // std::cout << v2.size() << std::endl;
-    // std::cout << v2[1] << std::endl;
-    // std::cout << v2[5] << std::endl;
-    // std::cout << v2[8] << std::endl;
+		// v2.push_back("sdff");
+		// std::cout << v2.size() << std::endl;
+		// v2.push_back("sdff");
+		// std::cout << v2.size() << std::endl;
+		// std::cout << v2[1] << std::endl;
+		// std::cout << v2[5] << std::endl;
+		// std::cout << v2[8] << std::endl;
 
 
 
